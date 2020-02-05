32dc34a169717617730a8dc241e007737978951b
aserk1997 <57926170+aserk1997@users.noreply.github.com>
2020-02-04 23:15:35 -0500

    Set up basic movement for Appletree
    Set up code for speed per frame for appletree and set it up to add movement code

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -3,14 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject   applePrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Speed that Appletree moves
+    private float speed = 8f;
+
+    //Distance where Appletree turns around
+    public float leftAndRightEdge = 10f;
+
+    //Rate of Appletree is being instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+
+        set
+        {
+            speed = value;
+        }
+    }
+
+    public float Speed1
+    {
+        get
+        {
+            return speed;
+        }
+
+        set
+        {
+            speed = value;
+        }
+    }
+
+    void Start() {
+        //Dropping Apples/Second
+
+    }
+
+    private void Update()
+    {
+        //Basic Movement
+        Vector3 pos = transform.position; //b
+    
+    pos.x += speed * Time.deltaTime; //c
+
+        transform.position = pos; //d
+     //Changing Direction
+    }
 }
