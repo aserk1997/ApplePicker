510ee0a9bb086c1e7ed595ea43fbe36e43deff99
aserk1997 <57926170+aserk1997@users.noreply.github.com>
2020-02-05 23:37:05 -0500

    Update Basket.cs
    Changed Script to make basket follow mouse

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Basket : MonoBehaviour {
+    private Vector3 pos;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Update() {
+        // Get screen position of mouse from input
+
+        Vector3 mousePos2D = Input.mousePosition; //a
+
+        //The Camera's z position sets how far the to push the mouse
+
+        mousePos2D.z = -Camera.main.transform.position.z; //b
+
+        //Convert 2D to 3D
+
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 pos = this.transform.position;
+
+        pos.x = mousePos3D.x;
+
+        this.transform.position = pos;
+
+    }
+
 }
